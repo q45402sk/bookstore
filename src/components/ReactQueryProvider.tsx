3c976bee7bc93c858911ad1f3e605c55b61d9ad5
Songeun <hannahkeeien@gmail.com>
2025-03-01 11:02:04 +0900

    Feat: 레이아웃 설정

---
+++ src/components/ReactQueryProvider.tsx
@@ -0,0 +1,24 @@
+'use client';
+import React, { PropsWithChildren } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+export default function Providers({ children }: PropsWithChildren) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
